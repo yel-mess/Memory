1e246dd5671b79f59704cb623eb9b496298433a2
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-08-31 12:14:15 +0200

    pair of blocks with same color

--- Assets/#Project/Scripts/LevelManager.cs
+++ Assets/#Project/Scripts/LevelManager.cs
@@ -12,7 +12,9 @@ public class LevelManager : MonoBehaviour
 
     public GameObject itemPrefab;
 
+    public Material[] materials;
     public ItemBehavior[] items; //liste d'itemBehavior
+    private Dictionary <int, Material> itemMaterial = new Dictionary<int, Material>();
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,7 @@ public class LevelManager : MonoBehaviour
         items = new ItemBehavior[row * col];
         int index = 0;
 
-        for(int x = 0; x < col; x++)
+        for(int x = 0; x < col; x++) {
             for(int z=0; z < row; z++) {
                 Vector3 position = new Vector3(x*gapCol, 0, z*gapRow);
                 GameObject item = Instantiate(itemPrefab, position, Quaternion.identity);
@@ -29,8 +31,33 @@ public class LevelManager : MonoBehaviour
                 items[index].manager = this;
                 index++;
             }
+        }
+        
+        GiveMaterials();
     }
 
+    private void GiveMaterials() {
+        List<int> possibilities = new List<int>();
+        for(int i=0; i < row * col; i++) {
+            possibilities.Add(i);
+        }
+
+        for (int i=0; i < materials.Length; i++) {
+            if(possibilities.Count < 2) break;
+            int idPos = Random.Range(0, possibilities.Count);
+            int id1 = possibilities[idPos];
+            possibilities.RemoveAt(idPos);
+
+            idPos = Random.Range(0, possibilities.Count);
+            int id2 = possibilities[idPos];
+            possibilities.RemoveAt(idPos);
+
+            itemMaterial.Add(id1, materials[i]);
+            itemMaterial.Add(id2, materials[i]);
+            items[id1].GetComponent<Renderer>().material = materials[i];
+            items[id2].GetComponent<Renderer>().material = materials[i];
+        }
+    }
     // Update is called once per frame
     void Update()
     {
